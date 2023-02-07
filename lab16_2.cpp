f6541d2ce179e3dd07aabaf1e27ea98b460e202e
Your Name <you@example.com>
2023-02-07 15:42:15 +0700

    lab16_2cc

--- lab16_2.cpp
+++ lab16_2.cpp
@@ -7,7 +7,7 @@ int main(){
 	cout << "Before: ";
 	for(int i = 0; i<10; i++) cout << data[i] << " ";
 	sort(&data[0],&data[5]);
-	sort(&data[6],&data[10]);
+	sort(&data[5],&data[10]);
 	cout << "\nAfter: ";
 	for(int i = 0; i<10; i++) cout << data[i] << " ";
 	return 0;
