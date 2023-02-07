6276c9d40843e200f6613f57dde3faa4325e0140
Your Name <you@example.com>
2023-02-07 16:30:41 +0700

    lab16_4 cc

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,19 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int x;
+	int n = rand()%4;
+	int o[] = {a,b,c,d};
+	for(int i=0;i<4;i++){
+		x = o[i];
+		o[i] = o[n];
+		o[n] = x;
+	}
+
+	a = o[0];
+	b = o[1];
+	c = o[2];
+	d = o[3];
+}
